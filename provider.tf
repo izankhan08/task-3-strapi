68a9a2eda278a560fa05ce41d76aa672baa6e4f7
Mohammad Izan Khan <98902714+izankhan08@users.noreply.github.com>
2026-02-06 10:23:57 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-     access_key = "AKIAQZRSUR64ULI7R6WF"
-  secret_key = "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"
+     access_key = ""
+  secret_key = ""
+
 
 }
